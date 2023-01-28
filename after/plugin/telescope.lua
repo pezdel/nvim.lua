4cf32b87e156613fafecefe4cd8f5c146d0e45ac
pezdel <adam.pezdel@gmail.com>
2023-01-28 13:29:34 -0600

    removed some stuff

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -33,19 +33,4 @@ require('telescope').setup({
         path_display = {"smart"},
         file_ignore_patterns = { ".git/", "node_modules" },
     },
-    pickers = {
-        find_files = {
-            mappings = {
-                n = {
-                    ["cd"] = function(prompt_bufnr)
-                        local selection = require("telescope.actions.state").get_selected_entry()
-                        local dir = vim.fn.fnamemodify(selection.path, ":p:h")
-                        require("telescope.actions").close(prompt_bufnr)
-                        -- Depending on what you want put `cd`, `lcd`, `tcd`
-                        vim.cmd(string.format("silent lcd %s", dir))
-                    end
-                }
-            }
-        },
-    }
 })
